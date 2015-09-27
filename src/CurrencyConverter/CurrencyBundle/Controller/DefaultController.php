fdbdbd9952838bf5c104e233f8a4e2ccb340e2a8
Daniel Suszek <danielsuszek1@gmail.com>
2015-09-27 20:21:41 +0200

    Convert works, no Ajax

--- src/CurrencyConverter/CurrencyBundle/Controller/DefaultController.php
+++ src/CurrencyConverter/CurrencyBundle/Controller/DefaultController.php
@@ -7,10 +7,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 // import new namespaces
 use CurrencyConverter\CurrencyBundle\Entity\Currency;
 use CurrencyConverter\CurrencyBundle\Form\CurrencyType;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {       
         // create empty model for Currency
         $currency = new Currency();
@@ -18,8 +19,40 @@ class DefaultController extends Controller
         $form = $this->createForm(new CurrencyType, $currency);
         
         // render view with form
+        $form->handleRequest($request);  
+        
+        if ($form->isValid()) {
+            // perform some action, such as saving the task to the database
+            $currencyInput = $form["currencyInput"]->getData();
+            
+            $convertedCurrency = $currency->convertCurrencyAction('RUB', 'PLN', $currencyInput);
+            
+            return $this->render('CurrencyConverterCurrencyBundle:Default:index.html.twig', array(
+                'form' => $form->createView(), 'currencyInput' => $currencyInput, 'convertedCurrency' => $convertedCurrency
+            ));
+        }
+        
         return $this->render('CurrencyConverterCurrencyBundle:Default:index.html.twig', array(
+            'form' => $form->createView(), 'currencyInput' => '', 'convertedCurrency' => ''
+        ));
+    }
+    
+    public function testFormAction(Request $request)
+    {
+        $currency = new Currency();
+        $form =  $this->createFormBuilder($currency)
+                ->add('currencyInput', 'integer', array('label' => 'RUB'))
+                ->add('save', 'submit', array('label' => 'Convert to PLN'))
+                ->getForm();    
+        
+        $form->handleRequest($request);  
+        
+        if ($form->isValid()) {                    
+            exit('Form is valid');
+        }
+        return $this->render('CurrencyConverterCurrencyBundle:Default:testForm.html.twig', array(
             'form' => $form->createView()
         ));
     }
+    
 }
--- src/CurrencyConverter/CurrencyBundle/Form/CurrencyType.php
+++ src/CurrencyConverter/CurrencyBundle/Form/CurrencyType.php
@@ -3,17 +3,30 @@ namespace CurrencyConverter\CurrencyBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
 
 class CurrencyType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('currencyInput', 'text', array('label' => 'RUB'));     
+        $builder->add('currencyInput', 'text', array('label' => 'RUB'))
+            ->add('save', 'submit', array('label' => 'Convert to PLN'))
+            ->getForm();     
         
         
     }
 
-       public function getName()
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'CurrencyConverter\CurrencyBundle\Entity\Currency'
+        ));
+    }
+
+    
+    public function getName()
     {
         return 'currency';
     }
